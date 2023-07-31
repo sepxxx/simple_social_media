28f06308161909e054b4aa91bd5036903dfdb61f
Jeraldine Davon <jeraldin@et-i6.kzn.21-school.ru>
2023-07-31 11:02:36 +0300

    from get/set to lomboks data annot

--- src/main/java/com/simple_social_media/entity/Post.java
+++ src/main/java/com/simple_social_media/entity/Post.java
@@ -1,8 +1,10 @@
 package com.simple_social_media.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name="post")
 public class Post {
     @Id
@@ -29,38 +31,6 @@ public class Post {
     public Post(String header, String text, String image_url) {
         this.header = header;
         this.text = text;
-        this.image_url = image_url;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getHeader() {
-        return header;
-    }
-
-    public void setHeader(String header) {
-        this.header = header;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getImage_url() {
-        return image_url;
-    }
-
-    public void setImage_url(String image_url) {
         this.image_url = image_url;
     }
 }
--- src/main/java/com/simple_social_media/entity/UserProfile.java
+++ src/main/java/com/simple_social_media/entity/UserProfile.java
@@ -4,10 +4,10 @@ import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import com.simple_social_media.entity.Post;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name="user_profile")
 public class UserProfile {
     @Id
@@ -87,37 +87,5 @@ public class UserProfile {
 
     public UserProfile() {
 
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
